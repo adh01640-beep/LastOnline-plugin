84379e710881d737eb7f71b68de2bc5b93a26cc6
adh01640-beep <adh01640@gmail.com>
2026-08-18 01:35:54 +0300

    Update build.gradle.kts

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -1,12 +1,7 @@
-plugins {
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.aliucord.plugin)
-}
-
-aliucord {
-    author("Adham", 0L)
-    description = "Shows last online presence timestamp."
-    version = "1.0.0"
-    deploy.set(true)
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.aliucord.com")
+    }
 }
