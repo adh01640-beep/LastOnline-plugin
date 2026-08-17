9d2d666dd8ee3203ab8edb28e1f10da0204a46a6
adh01640-beep <adh01640@gmail.com>
2026-08-17 22:49:51 +0300

    Create build.gradle.kts

---
+++ plugins/LastOnline/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.aliucord.plugin)
+}
+
+aliucord {
+    author("Adham", 0L)
+    description("Displays the last seen timestamp of a user on their profile.")
+    version("1.0.0")
+    deploy.set(true)
+}
